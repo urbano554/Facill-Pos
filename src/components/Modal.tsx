0be7ab179eea14c64a58de0b883b87bc01aa964f
luis urbano <urbanoluismanuel54@gmail.com>
2023-01-05 09:57:25 -0400

    Feature(test-git-revert)
    Change home text

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,6 @@
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 import { ModalProps } from '../common/entities';
 import { useAppSelector } from '../global/hooks';
